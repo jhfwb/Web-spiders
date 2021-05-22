04e4efd32ce7afa4e9a6a0e7ff55077ab0c653f4
xmind <527077832@qq.com>
2021-05-22 22:17:39 +0800

    测试

--- 简化操作.bat
+++ 简化操作.bat
@@ -5,7 +5,7 @@ title   Git Working
 cls 
 echo git’Àªß:  jhfwb
 echo git√‹¬Î:  jhfwb512.
-echo  ¡Ó≈∆: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
+echo  ¡Ó≈∆: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
 goto selectAll
 
 pause
