6f2896f25efb80fa9a6af549a84db217e5bd7018
xmind <527077832@qq.com>
2021-06-30 23:29:27 +0800

    pycharm commit2

---
+++ _xhr_tool/chromeRobot/-crapyPython/天眼查验证_消防.py
@@ -0,0 +1,119 @@
+import time
+from _utils.RUtils import tool
+
+from _xhr_tool._utils import relpath
+from _xhr_tool._utils.arr_caculate import remove_repeat_arr_inner_dict
+from _xhr_tool.csv_tools._utils.CsvTool import CsvTool
+import difflib
+from _xhr_tool.chromeRobot.src._chromeRobot_tool.dataUtils import DataUtils
+from _xhr_tool.chromeRobot.src._chromeRobot_tool.decoratorUtils import DecoratorEngine
+from _xhr_tool.chromeRobot.src._decorator._decorator import chrome_robot_excute, chrome_datas_catch
+from _xhr_tool.chromeRobot.src.domain.Action2 import Action
+from _xhr_tool.chromeRobot.src.domain.ChromeFactory import ChromeFactory
+from _xhr_tool.chromeRobot.src.domain.HighterAction2 import HigherAction
+from _xhr_tool.mysql.connect import MySqlOptions
+class Tianyancha:
+    savePath=relpath('../datasStore/天眼查/cccf.csv')
+    saveCsvHeads=['公司名','联系人','电话','地址', '城市', 'qq', '微信','邮箱','_url']
+    #初始化
+    def __init__(self,company,sqlOption):
+        self.company=company
+        self.sqlOption=sqlOption
+        pass
+    # @chrome_robot_excute
+    def test(self):
+            HigherAction().click_byName(cssStr='.sup-ie-company-header-child-1 .link-click.link-spacing',name='查看更多').\
+            selcetAction(action1=Action().find(cssStr='.phone',key='电话',mode='multiple',catchDate=True).clickXY(),
+                         action2=Action().find(cssStr='.sup-ie-company-header-child-1 span:nth-child(4)',key='电话',catchDate=True)
+                         ).excute(isSave=True)
+        # HigherAction().find(cssStr='.phone',key='电话',mode='multiple').excute(isSave=True)
+    @chrome_robot_excute
+    def web(self):
+        self._stopsign=-1
+        def _isStop(resp,self_stopsign):
+            if resp.datas=='我们只是确认一下你不是机器人，':
+                time.sleep(10)
+            else:
+                self_stopsign._stopsign=1
+        #判断是不是进入机器人状态
+        while self._stopsign==-1:
+            HigherAction().jumpBrowserTab(index=-1).find(cssStr='.captcha-title',catchDate=False,after_func=_isStop,after_func_args=[self]).\
+                excuteBlock(isSave=False)
+        #取出所有公司名称，然后逐个验证
+        HigherAction().jumpBrowserTab(index=-1).\
+            key_input(cssStr="input[type='search']",text=self.company).\
+            click(cssStr='.input-group-btn',loadNewPage=True).\
+            jumpBrowserTab(index=-1).\
+            click(cssStr='.search-result-single em',index=0,loadNewPage=True).\
+            jumpBrowserTab(index=-1). \
+            find(cssStr=".name>.link-click", key="客户",catchDate=True,ignoreErr=False). \
+            find(cssStr=".name", key="公司", catchDate=True). \
+            find(cssStr=".auto-folder", key="地址",catchDate=True). \
+            find(cssStr="tbody > tr:nth-child(11) > td:nth-child(2) > span", key="产品信息",catchDate=True). \
+            find(cssStr=".-breakall > tbody > tr:nth-child(1) > td:nth-child(4)", key="经营状况",catchDate=True). \
+            find(cssStr=".-breakall tr:nth-child(4) > td:nth-child(2)", key="实缴资本", catchDate=True). \
+            find(cssStr=".-breakall tr:nth-child(3) > td:nth-child(2)", key="注册资本", catchDate=True). \
+            find(cssStr=".-breakall  tr:nth-child(7) > td:nth-child(4)", key="行业",catchDate=True).\
+            find(cssStr=".email", key="邮箱",catchDate=True).\
+            find(cssStr=".company-link", key="公司网址",catchDate=True).\
+            findCurrentUrl(key='_url',catchDate=True).\
+            click_byName(cssStr='.sup-ie-company-header-child-1 .link-click.link-spacing', name='查看更多'). \
+            selcetAction(action1=Action().find(cssStr='.phone', key='电话', mode='multiple', catchDate=True).clickXY(),
+                         action2=Action().find(cssStr='.sup-ie-company-header-child-1 .label +div+span', key='电话',catchDate=True)).\
+            closeCurrentBroswserTab().\
+            excuteBlock(isSave=True)
+    @chrome_datas_catch
+    def dataSave(self,datas):
+        if datas=={}:
+            obj = self.sqlOption.getTableDict(table='company')
+            obj['数据状态'] = 'bug'
+            sqlOption.update_obj(table='company', obj=obj, uniqueCondition=[('公司', self.company)])
+            tool().print('出现bug，已经重新定义该数据')
+            return
+        if difflib.SequenceMatcher(None, datas['公司'], self.company).quick_ratio()>0.9:#比较数据库中的公司名称与天眼查的公司名称的相似度，达到0.9才会录进去
+            obj=self.sqlOption.getTableDict(table='company')
+            print(datas)
+            obj['公司']=datas['公司']
+            obj['公司简介']=datas['产品信息']
+            obj['地址']=datas['地址']
+            obj['经营状况']=datas['经营状况']
+            obj['注册资本']=datas['注册资本']
+            obj['实缴资本']=datas['实缴资本']
+            obj['行业']=datas['行业']
+            obj['客户']=datas['客户']
+            obj['邮箱']=datas.get('邮箱')
+            obj['公司网址']=datas.get('公司网址')
+            obj['电话集']=datas.get('电话')
+            obj['数据状态']='天眼查已查证'
+            if obj['电话集']!=None:
+                电话集=obj['电话集']
+                if type(电话集)!=type([]):
+                    电话集=[电话集]
+                if len(电话集)!=0:
+                    电话集=list(filter(lambda x:len(x)==11,电话集))[0:3]
+                    for i in range(0,len(电话集)):
+                        obj['电话'+str(i+1)]=电话集[i]
+            try:
+                sqlOption.update_obj(table='company',obj =obj,uniqueCondition=[('公司',self.company)])
+                tool().print('保存数据成功:' + str(obj))
+            except Exception:
+                obj = self.sqlOption.getTableDict(table='company')
+                obj['数据状态'] = 'bug'
+                sqlOption.update_obj(table='company', obj=obj, uniqueCondition=[('公司', self.company)])
+                tool().print('出现bug，已经重新定义该数据')
+        else:#相似度不够
+            obj=self.sqlOption.getTableDict(table='company')
+            obj['数据状态']='天眼查已查证_相似度不足'
+            sqlOption.update_obj(table='company', obj=obj, uniqueCondition=[('公司', self.company)])
+            tool().print('保存数据失败,相似度不足:' + str(self.company)+"与"+str(datas['公司']))
+if __name__ == '__main__':
+    chromeFactory = ChromeFactory()
+    sqlOption = MySqlOptions(host='localhost', user='root', password='512124632', database='消防公司产品')
+    print(sqlOption.getTableDict(table='company'))
+    datas=sqlOption.find_tables(table='company',conditions=[('数据状态','未知数据')],columns=['公司'])
+    # 需天眼查查验
+    for data in datas[1:]:
+        chromeFactory.register(Tianyancha(data[0],sqlOption))
+
+
+
---
+++ _xhr_tool/chromeRobot/-crapyPython/爱采购_消防.py
@@ -0,0 +1,109 @@
+import time
+from _utils.RUtils import tool
+from _xhr_tool.chromeRobot.src._decorator._decorator import chrome_robot_excute, chrome_datas_catch
+from _xhr_tool.chromeRobot.src.domain.Action2 import Action
+from _xhr_tool.chromeRobot.src.domain.ChromeFactory import ChromeFactory
+from _xhr_tool.chromeRobot.src.domain.HighterAction2 import HigherAction
+from _xhr_tool.chromeRobot.src.servlet.robot_thread import MyPipelineThread
+from _xhr_tool.csv_tools._utils.CsvTool import CsvTool
+from _xhr_tool.excuteEngine.Engine import ExcuteFuel
+from _xhr_tool.mysql.connect import MySqlOptions
+class Aaicaigou:
+    def __init__(self,key_word):
+        self.key_word = key_word
+        # self.savePath = '../datasStore/爱采购/爱采购爬虫_' + self.key_word + '.csv'
+        self.cycleTimes = -1
+        self.saveCsvHeads = ['公司名', '联系人', '电话', '地址', '城市', 'qq', '微信', '邮箱', '_url']
+        self.sqlOption=MySqlOptions(host='localhost', user='root', password='512124632', database='消防公司产品')
+    # @chrome_robot_excute
+    def test(self):
+        Action().scroll_browser_top_to_button(cssStr='.ivu-page-next',elemntName='下一页',timeOut=60).excuteBlock()
+        print('运行完毕')
+
+    @chrome_robot_excute
+    def web(self):
+        HigherAction().initWeb(url='https://b2b.baidu.com/').\
+            click(cssStr='.ivu-icon.ivu-icon-ios-close',ignoreErr=True).\
+            key_input(cssStr=".ivu-input", text=self.key_word, isClear=True). \
+            click(cssStr=".search-btn"). \
+            jumpBrowserTab(index=-1).excute()
+    @chrome_robot_excute
+    def webInit(self):#初始化
+        def test(response):
+            if response.datas=='' or response.datas==None:
+                self.cycleTimes=1
+            else:
+                self.cycleTimes=int(response.datas)
+        Action().scroll_browser_top_to_button(cssStr='.ivu-page-next',elemntName='下一页',timeOut=20).\
+            find(cssStr='.ivu-page-item a',index=-1,after_func=test). \
+            excuteBlock()
+    @chrome_robot_excute
+    def webRobot(self):
+        def crapy(response):
+            HigherAction().jumpBrowserTab(index=-1).\
+            findCurrentUrl(key='_url', catchDate=True).\
+            sendDatas(key_datas='公司名',datas=response.datas,catchDate=True).\
+            find(cssStr='.nav-item').selcetAction(
+                action1=HigherAction().click(cssStr='.nav-item:nth-child(4)',loadNewPage=True).jumpBrowserTab(index=-1).
+                find(key='城市', cssStr='.icons',ignoreErr=False, catchDate=True).
+                find(key='地址', cssStr='.section-container:nth-child(4)', catchDate=True).
+                find(key='联系人', cssStr='.section-container:nth-child(2) .item-container .value',catchDate=True).
+                find(key='电话', cssStr='.section-container:nth-child(2) .item-container:nth-child(2) .value',catchDate=True).
+                find(key='邮箱', cssStr='.section-container:nth-child(2) .item-container:nth-child(3) .value',catchDate=True).
+                find(key='qq', cssStr='.section-container:nth-child(2) .item-container:nth-child(4) .value',catchDate=True).
+                find(key='微信', cssStr='.section-container:nth-child(2) .item-container:nth-child(5) .value',catchDate=True).
+                sendDatas(key_datas='关键字',datas=self.key_word).closeCurrentBroswserTab()
+            ).closeCurrentBroswserTab().excuteRightNow(isSave=True)
+        for i in range(self.cycleTimes):
+            def _isClick(name):
+                _a=self.sqlOption.find(table='company',columns=['公司'],conditions=[('公司',name)])
+                if len(_a)!=0:
+                    print('该公司已存在:'+name)
+                    return False
+                else:
+                    return True
+            HigherAction().\
+            click_all_except_crapyedDatas2(cssStr='.s-content .p-card-desc-layout>div:last-child .name',func=crapy,before_click_func=_isClick).\
+            click(cssStr='.ivu-page-next',ignoreErr=False).scroll_browser_top_to_button(cssStr='.ivu-page-next',elemntName='下一页',timeOut=60).excuteBlock()
+            tool().print('...开始运行下一页...')
+        print('运行结束')
+    @chrome_datas_catch
+    def dataSave(self,datas):
+        # 1.将此数据保存到爬虫配置文件中。
+        obj={}
+        for saveCsvHead in self.saveCsvHeads:
+            obj.setdefault(saveCsvHead,datas.get(saveCsvHead))
+        #1.将公司信息存入数据库
+        obj1=self.sqlOption.getTableDict(table='company')
+        obj1['公司']=obj.get('公司名')
+        obj1['地址']=obj.get('地址')
+        obj1['关键词']=self.key_word
+        obj1['数据来源'] = '爱采购'
+        obj1['reliability'] = 1
+        if self.sqlOption.insert(table='company',objDict=obj1):
+            tool().print('保存数据到表company_成功:'+str(obj1),fontColor='green')
+        else:
+            tool().print('保存数据到表company_失败:'+str(obj1),fontColor='red')
+        #2.根据公司名查找其公司id
+        id=self.sqlOption.find(table='company',columns=['id'],conditions=[("公司",obj1['公司'])])
+        if id:
+            id=id[0]
+            obj2=self.sqlOption.getTableDict(table='customers')
+            obj2['qq号']=obj.get('qq')
+            obj2['邮箱']=obj.get('邮箱')
+            obj2['微信号']=obj.get('微信')
+            obj2['_url'] = obj.get('_url')
+            obj2['客户']=obj.get('联系人')
+            obj2['公司id']=id[0]
+            obj2['手机1']=obj.get('电话')
+            obj2['reliability']=1
+            if self.sqlOption.insert(table='customers',objDict=obj2):
+                tool().print('保存数据到表customers_成功:'+str(obj2),fontColor='green')
+            else:
+                tool().print('保存数据到表customers_失败:'+str(obj2),fontColor='red')
+if __name__ == '__main__':
+    arr=['消防水带','水带接口','消火栓箱','消防软管','室外消火栓','消防软管卷盘','多用水泵接合器','室内消防栓']
+    chromeFactory = ChromeFactory()
+    for a in arr:
+        chromeFactory.register(Aaicaigou(a))
+
